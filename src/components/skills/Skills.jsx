d869d96909087b7a79e29c68e8a627f7dc0fb163
cdamyx <chris@amyx.engineering>
2022-03-23 22:21:35 -0500

    image editing

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,16 +6,25 @@ import cssImage from '../../assets/css.png';
 import sassImage from '../../assets/sass.png';
 import javascriptImage from '../../assets/javascript.png';
 import reactImage from '../../assets/react.png';
-import bootstrapImage from '../../assets/bootstrap.png';
+import bootstrapImage from '../../assets/bootstrap2.png';
+import npmImage from '../../assets/npm-logo.png';
+import ejsImage from '../../assets/ejs.png';
 import nodeImage from '../../assets/node.png';
 import expressImage from '../../assets/express.png';
 import sqlImage from '../../assets/sql.png';
 import mongoImage from '../../assets/mongo.png';
+import apiImage from '../../assets/api.png';
 import herokuImage from '../../assets/heroku.png';
 import awsImage from '../../assets/aws.png';
+import dockerImage from '../../assets/docker.png'
 import gitImage from '../../assets/git.png';
+import githubImage from '../../assets/github.png'
 import linuxImage from '../../assets/linux.png';
 import windowsImage from '../../assets/windows.png';
+import puppeteerImage from '../../assets/puppeteer.png';
+import agileImage from '../../assets/agile.png';
+import postmanImage from '../../assets/postman.png';
+import restImage from '../../assets/rest.png';
 
 export default function Skills(){
 
@@ -24,23 +33,23 @@ export default function Skills(){
 		{
 			id: '1',
 			icon: 'fab fa-chrome fa-1x',
-			title: 'Front End',
-			desc: 'HTML5 | CSS3 | SASS | Javascript | React | Bootstrap',
-			img: [htmlImage, cssImage, sassImage, javascriptImage, reactImage, bootstrapImage]
+			title: 'Frontend',
+			desc: 'HTML | CSS | SASS | Javascript | React | Bootstrap | NPM | EJS',
+			img: [htmlImage, cssImage, sassImage, javascriptImage, reactImage, bootstrapImage, npmImage, ejsImage]
 		},
 		{
 			id: '2',
 			icon: 'fas fa-server fa-1x',
-			title: 'Back End',
-			desc: 'NodeJS | ExpressJS | SQL | mongoDB | APIs',
-			img: [nodeImage, expressImage, sqlImage, mongoImage]
+			title: 'Backend/etc.',
+			desc: 'Node | Express | SQL | mongoDB | API | AWS | Heroku | Docker',
+			img: [nodeImage, expressImage, sqlImage, mongoImage, apiImage, awsImage, herokuImage, dockerImage]
 		},
 		{
 			id: '3',
 			icon: 'fas fa-arrow-alt-circle-up fa-1x',
-			title: 'Deploy/etc.',
-			desc: 'Heroku | AWS | Git | Linux | Windows | Agile | System Design',
-			img: [herokuImage, awsImage, gitImage, linuxImage, windowsImage]
+			title: 'Other',
+			desc: 'Git | Github | Linux | Windows | Puppeteer | Postman | Agile | REST/SOAP',
+			img: [gitImage, githubImage, linuxImage, windowsImage, puppeteerImage, postmanImage, agileImage, restImage]
 		},
 	]
 	const handleClick = (way) => {
